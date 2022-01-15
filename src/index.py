4c7799b7ff109e67f8031da58731b7d7a3a2a076
Sergio Méndez <sergioarm.gpl@gmail.com>
2022-01-15 17:17:35 +0000

    feat: Added initial content to the application

---
+++ src/index.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+import os
+from Operations import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    msg = os.environ['MESSAGE']
+    ns = os.environ['NAMESPACE']
+    o = Operations()
+    return o.runningInfo(msg,ns)
+
+@app.route("/_health")
+def url_health():
+    return "Running"
+
+@app.route("/_version")
+def url_version():
+    return "1.0.6"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
