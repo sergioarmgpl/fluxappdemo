8aafcab7f70df89e9792b794f8de7e41f2006d93
Sergio Méndez <sergioarm.gpl@gmail.com>
2022-01-18 04:27:32 +0000

    feat: changed version

--- src/index.py
+++ src/index.py
@@ -17,7 +17,7 @@ def url_health():
 
 @app.route("/_version")
 def url_version():
-    return "23:41"
+    return "22:27"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
